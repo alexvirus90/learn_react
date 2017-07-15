6a025e6716aa7a195fc2afaaae48180cb7a5f1be
alex <a.kom90@mail.ru>
2017-07-15 15:32:17 +0300

    Working with ReactDOM.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hi');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+
+	render() {
+		return (
+			<div>
+				<h1>App works!</h1>
+				<h3>It's really working!</h3>
+			</div>
+		);
+	}
+}
+
+ReactDom.render(
+	<App />,
+	document.getElementById('app')
+);
