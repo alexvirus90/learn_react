8b914f3be102f036a0d8c6dbb975e31fff7ee41b
alex <a.kom90@mail.ru>
2017-07-17 20:40:41 +0300

    JSX Syntex.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDom from 'react-dom';
 
 class App extends React.Component{
 
+	btnOnClick(event) {
+		console.log("Button clicked!", event);
+	}
+
 	render() {
 		return (
-			<div>
+			<div /*style={{background: 'red'}} */ className="test">
 				<h1>App works!</h1>
 				<h3>It's really working!</h3>
+				<button onClick={this.btnOnClick}>{ this.props.children}</button>
 			</div>
 		);
 	}
 }
 
 ReactDom.render(
-	<App />,
+	<App>Click on me!</App>,
 	document.getElementById('app')
 );
