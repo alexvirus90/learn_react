851ee3279354c34ae62f363acde49c279d53b857
alex <a.kom90@mail.ru>
2017-07-17 21:44:35 +0300

    Component State.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 class App extends Component{
 
-	static propTypes = {
-		btnText: PropTypes.string.isRequired,
-		h1Text: PropTypes.string.isRequired,
-		newArray: PropTypes.array.isRequired
-	};
+	constructor(props) {
+		super(props);
 
-	static defaultProps = {
-		btnText: 'default props text'
-	};
+		this.state = {
+			text: ''
+		};
+	}
 
-	btnOnClick(event) {
-		console.log("Button clicked!", event);
+	inputOnChange(event) {
+		const text = event.target.value;
+		this.setState({ text });
 	}
 
 	render() {
-		console.log('array', this.props.newArray);
-		// console.log('Props', this.props);
 		return (
 			<div /*style={{background: 'red'}} */ className="test">
-				<h1>{ this.props.h1Text }</h1>
-				<h3>It's really working!</h3>
-				<button onClick={this.btnOnClick}>{ this.props.btnText /*|| 'Default text'*/}</button>
+				<h1>Hello</h1>
+				<input type="text" value={ this.state.text } onChange={ this.inputOnChange.bind(this) } />
 			</div>
 		);
 	}
 }
 
 ReactDom.render(
-<App /*btnText="click on me!"*/ h1Text="This is h1 text." newArray={[1, 2, 3,]}/>,
+	<App />,
 	document.getElementById('app')
 );
