c5be189d605fe9c5a1a4ce6d31a27b934ebc89d0
alex <a.kom90@mail.ru>
2017-08-06 20:14:43 +0300

    Create a page with list.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './app';
 
 import { HomePage, HomeRoutes } from './pages/home/index';
 import { ContactRoute } from './pages/contact/index';
+import { ListRoutes } from './pages/list/index';
 import ErrorPage from './pages/error/index';
 
 export default (
@@ -11,6 +12,7 @@ export default (
        <IndexRoute component={ HomePage } />
         { HomeRoutes }
         { ContactRoute }
+        { ListRoutes }
 
         <Route path='*' component={ ErrorPage } />
     </Route>
